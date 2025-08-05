73c7d6ac63b68e797e908d60bf76b6ba19ba4d64
mmcb2029be06 <b2029mmcarandang@pshs.edu.ph>
2025-08-05 08:55:14 +0800

    7th commit

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@
 
 var age = 14;
 var maxAge = 120;
-var numPerDay = 5;
+var numPerDay = 3;
 var totalRequired = (numPerDay * 365) * (maxAge - age);
 
-document.write("You will need " + totalRequired + " to last until you turn the ripe old age of " + maxAge);
+document.write("You will need " + totalRequired + " bowls of tsamporado to last until you turn the ripe old age of " + maxAge);
