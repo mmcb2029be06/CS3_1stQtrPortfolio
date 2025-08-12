998f5aa01e1edfd10377a6958b952c697fd3aca3
mmcb2029be06 <b2029mmcarandang@pshs.edu.ph>
2025-08-12 08:55:16 +0800

    9TH COMMIT

--- public/script.js
+++ public/script.js
@@ -2,12 +2,12 @@
 
 
 
-var age = window.prompt("What is your age? ", "0");
-var maxAge = window.prompt("Until how long will you live? ", "0");
-var numPerDay = window.prompt("How many bowls of tsamporado will you eat per day? ", "0")
+//ar age = window.prompt("What is your age? ", "0");
+//var maxAge = window.prompt("Until how long will you live? ", "0");
+//var numPerDay = window.prompt("How many bowls of tsamporado will you eat per day? ", "0")
 
 if (confirm("Are you sure you're good with the values?")){
-    var totalRequired = (parseInt(numPerDay) * 365) * (parseInt(maxAge) - parseInt(age));
+    var totalRequired = (parseInt(perday) * 365) * (parseInt(mxage) - parseInt(age));
     document.write("You will need " + totalRequired + " bowls of tsamporado to last until you turn the ripe old age of " + maxAge);
 } else {
     alert("Run the code again. ");
